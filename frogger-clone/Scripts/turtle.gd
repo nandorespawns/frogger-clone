61ac528e315bd4baa137dd7ba7e0826e260cca58
nando <castill0jcz@hotmail.com>
2025-09-06 18:35:09 -0700

    updated turtle

--- frogger-clone/Scripts/turtle.gd
+++ frogger-clone/Scripts/turtle.gd
@@ -2,30 +2,26 @@ extends Node2D
 
 var tile_size = 16
 
-var speed = 0.3
+var speed = 10
 
 var direction = {
 	"right": speed,
 	"left": -speed
 }
 
-
-
 @export var direction_picked: String
 @onready var hitbox: Area2D = $hitbox
-var area_array = []
-
 
 func _ready() -> void:
 	position = position.snapped(Vector2.ONE * tile_size)
 	position += Vector2.ONE * tile_size/2
 
-func _physics_process(_delta: float) -> void:
-	position.x += direction[direction_picked]
+func _physics_process(delta: float) -> void:
+	position.x += direction[direction_picked] * delta
 	
-	if hitbox.get_overlapping_areas():
-		var areas = hitbox.get_overlapping_areas()
-		for area in areas:
-			var player = area.get_parent()
-			player.position.x += direction[direction_picked]
+	var areas = hitbox.get_overlapping_areas()
+	
+	for area in areas:
+		var player = area.get_parent()
+		player.position.x += direction[direction_picked] * delta
 	
